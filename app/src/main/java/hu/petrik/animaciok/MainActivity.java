8e2885762ba44c2446a5d18ad8216fe1b6aabb29
Sárándi <sarandi.balint@gmail.com>
2022-10-21 08:24:30 +0200

    init method

--- app/src/main/java/hu/petrik/animaciok/MainActivity.java
+++ app/src/main/java/hu/petrik/animaciok/MainActivity.java
@@ -3,12 +3,24 @@ package hu.petrik.animaciok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button buttonBounce,buttonFadeIn, buttonFadeOut, buttonRotate,buttonZoomIn, buttonZoomOut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
+
+    private void init(){
+        buttonBounce = findViewById(R.id.buttonBounce);
+        buttonFadeIn = findViewById(R.id.buttonFadeIn);
+        buttonFadeOut = findViewById(R.id.buttonFadeOut);
+        buttonRotate = findViewById(R.id.buttonRotate);
+        buttonZoomIn = findViewById(R.id.buttonZoomIn);
+        buttonZoomOut = findViewById(R.id.buttonZoomOut);
     }
 }
